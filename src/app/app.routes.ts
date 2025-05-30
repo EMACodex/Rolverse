ffdde936bdb37b3e56f407bca3a6477bd58862cb
Erik <150074240+EMACodex@users.noreply.github.com>
2025-05-30 20:27:53 +0200

    homepage añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+    {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
